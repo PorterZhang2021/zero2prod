4fb0f9526b0f384e190cc677fb87a6966651749e
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-04 18:10:31 +0800

    test: refactor test case

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
@@ -14,6 +14,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await
+async fn spawn_app()  {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
