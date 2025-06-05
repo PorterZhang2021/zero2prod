b3285b1862227362b7fa3430716322d457c1cee0
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-05 16:35:52 +0800

    feat: add log plugin

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::new().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection_pool = PgPool::connect(&configuration.database.connection_string()).await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
         .expect("Failed to connect to Postgres");
     run(listener, connection_pool)?.await
 }
