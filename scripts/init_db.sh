87324da0de923558ed8d5b54ebc80aeca21ae659
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-05 10:45:51 +0800

    build: add init_db.sh

---
+++ scripts/init_db.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+#if ! [ -x "$(command -v psql)" ]; then
+#  echo >&2 "Postgres is not installed."
+#  exit 1
+#fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 " cargo install sqlx-cli --features postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD=${POSTGRES_PASSWORD:=password}
+DB_NAME=${POSTGRES_DB:=newsletter}
+DB_PORT=${POSTGRES_PORT:=5432}
+
+docker run \
+      -e POSTGRES_USER=${DB_USER} \
+      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+      -e POSTGRES_DB=${DB_NAME} \
+      -p ${DB_PORT}:5432 \
+      -d postgres \
+      postgres -N 1000
+
+export PGPASSWORD=${DB_PASSWORD}
+until psql -h "localhost" -U "${DB_USER}" -d "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sql database create
