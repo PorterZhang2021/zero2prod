f2c09a77bc6e3f0dbd37cdc0ad03f249a009a672
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-05 10:59:36 +0800

    build: add migrate content

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,10 +2,13 @@
 set -x
 set -eo pipefail
 
-#if ! [ -x "$(command -v psql)" ]; then
-#  echo >&2 "Postgres is not installed."
-#  exit 1
-#fi
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Postgres is not installed."
+  echo >&2 "Use:"
+  echo >&2 "  brew install postgresql"
+  echo >&2 "to install it."
+  exit 1
+fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
@@ -21,13 +24,16 @@ DB_PASSWORD=${POSTGRES_PASSWORD:=password}
 DB_NAME=${POSTGRES_DB:=newsletter}
 DB_PORT=${POSTGRES_PORT:=5432}
 
-docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p ${DB_PORT}:5432 \
-      -d postgres \
-      postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p ${DB_PORT}:5432 \
+        -d postgres \
+        postgres -N 1000
+fi
 
 export PGPASSWORD=${DB_PASSWORD}
 until psql -h "localhost" -U "${DB_USER}" -d "${DB_PORT}" -d "postgres" -c '\q'; do
@@ -38,4 +44,7 @@ done
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
-sql database create
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to run tests."
