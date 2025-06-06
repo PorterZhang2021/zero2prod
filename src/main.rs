78fd627dac992c41cece0f9d59a9519fdc62acc6
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-06 18:58:18 +0800

    refactor: refactor to support application environment

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to Postgres");
