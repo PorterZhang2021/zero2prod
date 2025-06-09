05641e81adcd665ee63d38087b737f181bd9eb79
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-09 10:54:22 +0800

    chore: 1. support require_ssl 2. add param require_ssl in yaml 3. refactor database configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -17,7 +16,6 @@ async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(address)?;
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string())
-        .expect("Failed to connect to Postgres");
+        .connect_lazy_with(configuration.database.with_db());
     run(listener, connection_pool)?.await
 }
