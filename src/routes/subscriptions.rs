e9c84153ec3297edcba1d6f853707f2d1a6dd58d
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-11 11:02:35 +0800

    feat: use subscriber_email to replace string

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,11 @@
-
+use crate::domain::new_subscriber::NewSubscriber;
+use crate::domain::subscriber_email::SubscriberEmail;
+use crate::domain::subscriber_name::SubscriberName;
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
 use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
-use crate::domain::new_subscriber::NewSubscriber;
-use crate::domain::subscriber_name::SubscriberName;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -25,10 +25,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let new_subscriber = NewSubscriber {
-        name,
-        email: form.0.email,
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
     };
+    let new_subscriber = NewSubscriber { name, email };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -58,7 +59,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
