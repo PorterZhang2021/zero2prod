80bf6a135006415c4632eae1b6e9cb068eb913ee
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-27 10:58:18 +0800

    tmp

---
+++ src/routes/subscriptions_confirm.rs
@@ -0,0 +1,12 @@
+use actix_web::HttpResponse;
+use actix_web::web::Query;
+
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    subscription_token: String,
+}
+
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(_parameters))]
+pub async fn confirm(_parameters: Query<Parameters>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
