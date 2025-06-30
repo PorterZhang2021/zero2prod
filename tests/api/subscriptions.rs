63ea5cd668844898aac2b1442420be822f917b05
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-06-30 11:02:17 +0800

    refactor: refactor confirmation_links test case

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -7,15 +7,15 @@ use wiremock::{Mock, ResponseTemplate};
 async fn subscribe_returns_a_200_for_valid_from_data() {
     let app = spawn_app().await;
     let body = "name=Ursula%20Le%20Guin&email=ursula_le_guin%40gmail.com";
-    
+
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-    
+
     let response = app.post_subscriptions(body.into()).await;
-    
+
     assert_eq!(200, response.status().as_u16());
 }
 
@@ -23,20 +23,20 @@ async fn subscribe_returns_a_200_for_valid_from_data() {
 async fn subscribe_persists_the_new_subscriber() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    
+
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-    
+
     app.post_subscriptions(body.into()).await;
 
     let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch inserted value");
-    
+
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
     assert_eq!(saved.status, "pending_confirmation");
@@ -113,17 +113,7 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     app.post_subscriptions(body.into()).await;
 
-    let email = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email.body).unwrap();
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|link| *link.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
-    let text_link = get_link(&body["TextBody"].as_str().unwrap());
-    assert_eq!(html_link, text_link);
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+    assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
